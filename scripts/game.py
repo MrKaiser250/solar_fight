dd5fcc5aafa77f80312a1d1335c750287cd2f9ae
MrKaiser2501 <guhilofoohe01@mail.ru>
2024-03-19 20:49:50 +0300

    Музыка, анимации

--- scripts/game.py
+++ scripts/game.py
@@ -1,12 +1,15 @@
 from re import T
+from turtle import back
 import pygame
 import math
 import copy
+import math
 
-global WIDTH, HEIGHT, FPS, BLACK, WHITE, SPEED, running
+global WIDTH, HEIGHT, FPS, BACKGROUND, WHITE, SPEED, running
 global characters
 
 pygame.init()
+pygame.mixer.init()
 
 class Character:
     def __init__(self, ids=0, name="", image=["", 100, 100, 0], coord = [0, 0], speed = 3, flip = 0, direct = [0, 0], acc = [0, 0]):
@@ -43,9 +46,8 @@ class Character:
 clock = pygame.time.Clock()
 WIDTH, HEIGHT = 1280, 720
 FPS = 60
-BLACK = (0,0,50)
+BACKGROUND = [0,0,50,0]
 WHITE = (255,255,255)
-
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Solar Fight v0.2")
 
@@ -57,14 +59,20 @@ characters = [
     Character(1, "Sun", [imgt+"sun.png", 200, 200, 0], [500,500], 3)
     ]
 
-    
+# Sounds
+sd = "sounds\\"
+sounds = [
+    pygame.mixer.Sound(sd+"music.mp3")
+    ]
 
 def nframe():
-    global BLACK, WHITE
-    global characters, textures
-    win.fill(BLACK)
+    global BACKGROUND
+    global characters
+    win.fill((BACKGROUND[0], BACKGROUND[1], BACKGROUND[2]))
+    vert_anim = 25*math.sin( (timer_curr[1] - timer_last[1])/1000*math.pi*2 )
+    hor_anim = 10*math.sin( (timer_curr[1] - timer_last[1])/2000*math.pi*2 )
     for char in characters:
-        win.blit(char.texture, (char.coord[0], char.coord[1]))
+        win.blit(char.texture, ( char.coord[0]+hor_anim, char.coord[1]+vert_anim) )
     pygame.display.flip()
 
 def custom_delay(duration):
@@ -85,19 +93,45 @@ def exit_func():
       
 
 
-# Game Code
+# --=Game~Code=--
+
+sounds[0].play()
+
+timer_last = [pygame.time.get_ticks(), pygame.time.get_ticks()]
+timer_interval = [(sounds[0].get_length()+1)*1000, 2000]
 running = True
 while running:
+    timer_curr = [pygame.time.get_ticks(), pygame.time.get_ticks()]
+    # Background animation
+    if timer_curr[1] - timer_last[1] > timer_interval[1]:
+        timer_last[1] = timer_curr[1]
+
     exit_func()
     nframe()
     clock.tick(FPS)
     
+    # Music
+    if timer_curr[0] - timer_last[0] > timer_interval[0]:
+        sounds[0].play()
+        timer_last[0] = timer_curr[0]
+
+    # Background color animation
+    if BACKGROUND[3] == 0:
+        BACKGROUND[2] += 1
+    elif BACKGROUND[3] == 1:
+        BACKGROUND[2] -= 1  
+    if BACKGROUND[3] == 0 and BACKGROUND[2] >= 120:
+        BACKGROUND[3] = 1
+    elif BACKGROUND[3] == 1 and BACKGROUND[2] <= 30:
+        BACKGROUND[3] = 0
+
+    # Character in-game processing
     for char in characters:     
         
         char.acc = [0, 0]
 
 
-        # Mr.Black control
+        # Mr.Black control exclusive
         if char.ids == 8:
             keys = pygame.key.get_pressed()
             if keys[pygame.K_w]:
@@ -129,13 +163,13 @@ while running:
             char.direct[1] /= modulo
         
         if char.direct[0]>=0:
-            char.coord[0] += math.floor(char.direct[0] * char.speed)
+            char.coord[0] += char.direct[0] * char.speed
         else:
-            char.coord[0] += math.ceil(char.direct[0] * char.speed)
+            char.coord[0] += char.direct[0] * char.speed
         if char.direct[1]>=0:
-            char.coord[1] += math.floor(char.direct[1] * char.speed)
+            char.coord[1] += char.direct[1] * char.speed
         else:
-            char.coord[1] += math.ceil(char.direct[1] * char.speed)
+            char.coord[1] += char.direct[1] * char.speed
         
         if char.coord[0]+char.width>WIDTH:
             char.coord[0] = WIDTH - char.width
