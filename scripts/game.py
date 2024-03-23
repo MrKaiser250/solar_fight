524cff83c42815714f859e33c18389ecc14c6a09
MrKaiser2501 <guhilofoohe01@mail.ru>
2024-03-24 00:23:53 +0300

    Атака вОлнами и волнАми

--- scripts/game.py
+++ scripts/game.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from re import I, T
 from turtle import back
 import pygame
@@ -48,6 +49,44 @@ class Character:
         new_char.name_render = font_name.render(new_char.name, True, WHITE)
         return new_char
 
+
+class Projectile:
+    def __init__(self, ids=0, name="", image=["", 100, 100], coord=[0, 0], speed=3, direct=[0, 0], rotation = 0, acc=[0, 0], death_timer = 100):
+        self.ids = ids
+        self.name = name
+        self.image = list(image)
+        img = pygame.image.load(image[0])
+        img = pygame.transform.scale(img, (image[1], image[2]))
+        self.texture = img
+        self.width = image[1]
+        self.height = image[2]
+        self.coord = list(coord)
+        self.speed = speed
+        self.direct = list(direct)
+        self.acc = list(acc)
+        self.death_timer = death_timer
+        self.rotation = rotation
+        self.birth_time = pygame.time.get_ticks()
+        
+    def __deepcopy__(self, memo=None):
+        memo = memo or {}
+        new_proj = Projectile(
+            ids=copy.deepcopy(self.ids, memo),
+            name=copy.deepcopy(self.name, memo),
+            image=copy.deepcopy(self.image, memo),
+            coord=copy.deepcopy(self.coord, memo),
+            speed=copy.deepcopy(self.speed, memo),
+            direct=copy.deepcopy(self.direct, memo),
+            rotation=copy.deepcopy(self.rotation, memo),
+            acc=copy.deepcopy(self.acc, memo),
+            death_timer=copy.deepcopy(self.death_timer, memo)
+        )
+        return new_proj
+
+    def direct_with_rotation(self):
+        self.direct[0] = math.cos(math.radians(self.rotation))
+        self.direct[1] = math.sin(math.radians(self.rotation))
+
 clock = pygame.time.Clock()
 WIDTH, HEIGHT = 1280, 720
 FPS = 60
@@ -62,12 +101,23 @@ ANIM_Y = 25
 # Characters and textures
 imgt = "images\\"
 characters_data = [
-    Character(8, "Mr.Black", [imgt+"andr.png", 100, 100, 0], [100,100], 4),
+    Character(8, "Mr.Black", [imgt+"andr.png", 120, 120, 0], [100,100], 4),
     Character(1, "Sun", [imgt+"sun.png", 200, 200, 0], [500,500], 3, [True, 600, 600]),
-    Character(4, "Skufislav", [imgt+"neStas.png", 100, 100, 0], [250,100], 4)
+    Character(4, "Skufislav", [imgt+"stas.png", 80, 80, 0], [250,100], 4)
+    ]
+
+projectiles_data = [
+    Projectile(1, "Wave", [imgt+"wave.png", 40, 20], [0,0], 12, [0,0], 0, [0,0], 10000),
+    Projectile(1, "Wave", [imgt+"wave.png", 40, 20], [0,0], 15, [0,0], 0, [0,0], 10000)
+    ]
+
+attack_data_data = [
+    [1,100,3,3,2000,7],
+    [2,50,100,100,5000,1]
     ]
 
 characters = copy.deepcopy(characters_data)
+projectiles = []
 
 # Textures
 textures = [
@@ -92,6 +142,8 @@ def nframe():
         win.blit(char.texture, ( char.coord[0]+hor_anim, char.coord[1]+vert_anim) )
     for char in characters:
         win.blit(char.name_render, ( char.coord[0]+char.width//2-char.name_render.get_width()//2+hor_anim, char.coord[1]-char.name_render.get_height()+vert_anim ))
+    for proj in projectiles:
+        win.blit(proj.texture, ( proj.coord[0]+hor_anim, proj.coord[1]+vert_anim ) )
     pygame.display.flip()
 
 def custom_delay(duration):
@@ -119,15 +171,82 @@ def distance(x1,y1,x2,y2):
 def distanceTO(char1, x2, y1):
     return ((char1.coord[0]+char1.width/2-x2)**2 + (char1.coord[1]+char1.height/2-y2)**2)**0.5
 
+def closestEnemy(characters, char):
+    min_distance = float('inf')
+    closest_enemy = None
+    
+    for char2 in characters:
+        if char2.ids != char.ids:  # Ensure char2 is not the same character
+            distance = distanceCHAR(char2, char)
+            if distance < min_distance:
+                min_distance = distance
+                closest_enemy = char2
+                
+    return closest_enemy
+
+def angle(x, y):
+    angle_rad = math.atan2(y, x)
+    angle_deg = math.degrees(angle_rad)
+    if angle_deg < 0:
+        angle_deg += 360
+    return angle_deg
+
+def attack(data, char):
+    global projectiles
+    if data[0]==1:
+        if len(characters)>1:
+            target = closestEnemy(characters, char)
+            deltaX = target.coord[0] + target.width / 2 - char.coord[0] - char.width / 2
+            deltaY = target.coord[1] + target.height / 2 - char.coord[1] - char.height / 2
+            proj = copy.deepcopy(projectiles_data[0])
+            proj.rotation = angle(deltaX, deltaY)
+            proj.direct_with_rotation()
+            proj.texture = pygame.transform.rotate(proj.texture, -proj.rotation)
+            proj.coord[0] = char.coord[0] - proj.texture.get_width()/2 + char.width/2
+            proj.coord[1] = char.coord[1] - proj.texture.get_height()/2 + char.height/2
+            projectiles.append(proj)
+    if data[0]==2:
+        target = closestEnemy(characters, char)
+        deltaX = target.coord[0] + target.width / 2 - char.coord[0] - char.width / 2
+        deltaY = target.coord[1] + target.height / 2 - char.coord[1] - char.height / 2
+        proj = copy.deepcopy(projectiles_data[1])
+        proj.rotation = angle(deltaX, deltaY)
+        proj.direct_with_rotation()
+        proj.texture = pygame.transform.rotate(proj.texture, -proj.rotation)
+        proj.coord[0] = char.coord[0] - proj.texture.get_width()/2 + char.width/2
+        proj.coord[1] = char.coord[1] - proj.texture.get_height()/2 + char.height/2
+        projectiles.append(proj)
+        
+        
+
+def attack_interval(data, char):
+    global attack_data_data
+    if data[5] <= 0:
+        data2 = random.choice(attack_data_data)
+        for i in range(len(data2)):
+            data[i]=data2[i]
+        return data[4]
+    elif data[3] <= 0:
+        data[3] = data[2]
+        data[5] -= 1
+        return data[4]
+    else:
+        data[3] -= 1
+        attack(data, char)
+        return data[1]
+
+
 # --=Game~Code=-- #
 
 sounds[0].play()
 
-timer_last = [pygame.time.get_ticks(), pygame.time.get_ticks()]
-timer_interval = [(sounds[0].get_length()+1)*1000, 2000]
+timer_last = [pygame.time.get_ticks(), pygame.time.get_ticks(), pygame.time.get_ticks()]
+timer_interval = [(sounds[0].get_length()+1)*1000, 2000, 0]
+attack_data = [0, 0, 0, 0, 0, 0] # Attack type, time between units, number of units, number of units (curr), time between attacks, number of attacks
+attack_data2 = []
 running = True
 while running:
-    timer_curr = [pygame.time.get_ticks(), pygame.time.get_ticks()]
+    timer_curr = [pygame.time.get_ticks(), pygame.time.get_ticks(), pygame.time.get_ticks()]
     # Background animation
     if timer_curr[1] - timer_last[1] > timer_interval[1]:
         timer_last[1] = timer_curr[1]
@@ -156,6 +275,10 @@ while running:
     # Character in-game processing
     for char in characters:     
         
+        if char.ids == 1 and timer_curr[2] - timer_last[2] > timer_interval[2]:
+            timer_last[2] = timer_curr[2]
+            timer_interval[2] = attack_interval(attack_data, char)
+
         char.acc = [0, 0]
         control_check = False
 
@@ -279,6 +402,18 @@ while running:
                     if distanceCHAR(char, char2) < 5:
                         characters.remove(char)
                         sounds[1].play()
+    
+    for proj in projectiles:
+        if pygame.time.get_ticks() - proj.birth_time > proj.death_timer:
+            projectiles.remove(proj)
+        else:            
+            proj.direct[0] += proj.acc[0]
+            proj.direct[1] += proj.acc[1]
+            proj.coord[0] += proj.direct[0] * proj.speed
+            proj.coord[1] += proj.direct[1] * proj.speed
+            
+        
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_r]:
         characters = copy.deepcopy(characters_data)
